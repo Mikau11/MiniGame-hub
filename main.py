ab77a70863a3581e8b10658815cd399e46f852c7
Mikau11 <elliotdinley@gmail.com>
2026-01-25 16:55:39 +0000

    Made a better start up and check if 1st time

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import password
+import time
 import mainmenu
+import thefiles
+import name
 
+
+
+print("Python Computer")
+time.sleep(0.5)
+print("2025-2026")
+time.sleep(0.5)
+print("Mini-Game hub")
+time.sleep(1)
+print("Starting........")
+time.sleep(5)
 print("*****Welcome to the Computer*****")
-password.password()
-mainmenu.menu()
+if thefiles.firts == "1":
+    name.name_password()
+else:
+    mainmenu.menu()
